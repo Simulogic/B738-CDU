4cbc74910295974b1b9cd2e23b5ce531d7990dd9
minestarnl <bramkoene@gmail.com>
2023-09-09 22:24:57 +0200

    Change to React

--- src/shims-vue.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import { ComponentCustomProperties } from "vue";
-import { Store } from "vuex";
-
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
