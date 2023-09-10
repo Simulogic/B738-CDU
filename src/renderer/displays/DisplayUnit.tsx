dc30f4a52af644643f2d43e63e4480b3ff0b982d
minestarnl <bramkoene@gmail.com>
2023-09-10 20:46:24 +0200

    Added release/app to git

--- src/renderer/displays/DisplayUnit.tsx
+++ src/renderer/displays/DisplayUnit.tsx
@@ -1,4 +1,25 @@
-export default function DisplayUnit(props) {
-  let location = props.location;
+interface DisplayUnitProps {
+  location: string;
+}
+
+export default function DisplayUnit(props: DisplayUnitProps) {
+  const { location } = props;
+  switch (location) {
+    // Lets check firstly what display location is being requested
+    case 'capt_outer':
+      return <div>Capt Outer</div>;
+    case 'capt_inner':
+      return <div>Capt Inner</div>;
+    case 'fo_inner':
+      return <div>FO Inner</div>;
+    case 'fo_outer':
+      return <div>FO Outer</div>;
+    case 'eicas_upper':
+      return <div>Eicas Upper</div>;
+    case 'eicas_lower':
+      return <div>Eicas Lower</div>;
+    default:
+      break;
+  }
   return <div>displayUnit</div>;
 }
